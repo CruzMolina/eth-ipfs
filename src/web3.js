f9989bef3475b48c3e4f6bfdcc7e3087912e77d3
Cruz Molina <Des1derata@192.168.1.11>
2018-04-10 17:32:11 -0700

    updated web3

--- src/web3.js
+++ src/web3.js
@@ -1,9 +1,19 @@
-// overrides metamask v0.2 for 1.0 version. 
+// overrides metamask v0.2 for 1.0 version.
 // 1.0 allows async and await instead of promises
 
+import Web3 from "web3";
 
-import Web3 from 'web3';
+let web3;
 
-const web3 = new Web3(window.web3.currentProvider);
+if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
+  // We are in the brwoser and metamask is running
+  web3 = new Web3(window.web3.currentProvider);
+} else {
+  // We are on the server OR the user is not running metamask
+  const provider = new Web3.providers.HttpProvider(
+    "https://rinkeby.infura.io/DZQCAMdvawWxFyA6OOtD"
+  );
+  web3 = new Web3(provider);
+}
 
 export default web3;
